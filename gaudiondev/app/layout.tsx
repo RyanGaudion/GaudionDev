5999836568271974937fbc7e4e8c8c38df196210
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-17 22:37:22 +0000

    Added analytics

--- gaudiondev/app/layout.tsx
+++ gaudiondev/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import Script from 'next/script'
+
 
 export const metadata = {
   title: 'Gaudion.Dev',
@@ -12,6 +14,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en-GB">
+      {/* Google Analytics*/}
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-9N7KFV3730"/>
+      <Script id='google-analytics' strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-9N7KFV3730', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
+      {/* -- End - Google Analytics -- */}
+
+
       <body className='bg-gray-900 text-white'>{children}</body>
     </html>
   )
