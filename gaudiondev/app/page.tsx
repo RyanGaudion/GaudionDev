9845c8264c28065e95324106e650213e895222ea
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-17 08:37:25 +0000

    Migrated to nextjs13

--- gaudiondev/app/page.tsx
+++ gaudiondev/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
-  return (
-    <main>
+    return (
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </main>
-  )
-}
+    )
+  }
+  
