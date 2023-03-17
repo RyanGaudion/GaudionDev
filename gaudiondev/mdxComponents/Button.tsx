4b33f177a8b582303b234c5314921cb127e1c639
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-17 11:01:56 +0000

    Typescript issues resolved

--- gaudiondev/mdxComponents/Button.tsx
+++ gaudiondev/mdxComponents/Button.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-const Button = ({ text }) => {
+interface ButtonProps{
+    text: string
+}
+
+const Button = ({ text }: ButtonProps) => {
     return (
       <button
         className="btn btn-primary"
-        onClick={event => event.target.innerText = 'You clicked me!'}
+        onClick={event => (event.target as HTMLElement).innerText = 'You clicked me!'}
       >
         {text}
       </button>
