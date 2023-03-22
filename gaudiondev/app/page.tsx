63a23db5b31cd8586ded6c608fe00bcb35dd5fc6
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-22 17:10:00 +0000

    not sure

--- gaudiondev/app/page.tsx
+++ gaudiondev/app/page.tsx
@@ -1,6 +1,8 @@
 import BlogPreviewComponent from '@/components/BlogPreview';
 import { getAllBlogs } from '@/lib/blogHelper';
 
+import Link from "next/link"
+
 export default function Home() {
 
   //Static by default
@@ -11,6 +13,18 @@ export default function Home() {
       <h1 className="text-4xl font-bold">
         Ryan Gaudion
       </h1>
+
+      <section className='py-10'>
+        <h2 className='text-3xl font-bold'>
+          Socials
+        </h2>
+
+        <div className='py-4 flex flex-col gap-4'>
+          <Link href="https://www.youtube.com/@RyanGaudion">YouTube</Link>
+          <Link href="https://twitter.com/Ryan_Gaudion">Twitter</Link>
+          <Link href="https://github.com/RyanGaudion/">GitHub</Link>
+        </div>
+      </section>
 
 
       <section className='py-10'>
@@ -23,8 +37,6 @@ export default function Home() {
             blog && <BlogPreviewComponent key={index} blog={blog}/>
           ))}
         </div>
-          
-        
       </section>
 
       
