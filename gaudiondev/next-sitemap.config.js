b1e26fe2603c76faddd213013081735ca53391b1
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-24 09:54:40 +0000

    Added sitemap generator

---
+++ gaudiondev/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://gaudion.dev',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
