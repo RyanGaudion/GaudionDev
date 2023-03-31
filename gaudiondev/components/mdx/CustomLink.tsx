fe89f6a775c903ae5d43a92defc7983a9fdbc21b
Ryan Gaudion <43444172+RyanGaudion@users.noreply.github.com>
2023-03-31 16:22:38 +0100

    Added table of content (manual)

--- gaudiondev/components/mdx/CustomLink.tsx
+++ gaudiondev/components/mdx/CustomLink.tsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 
 export default function CustomLink(props : any){
     const href = props.href;
-    const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+    const isInternalLink = href && (href.startsWith('/'));// || href.startsWith('#'));
+    const isAnchorLink = href && (href.startsWith('#'));
+
+    //TODO - Change back Url # handling not supported yet - https://beta.nextjs.org/docs/app-directory-roadmap
 
     if (isInternalLink) {
       return (
@@ -10,6 +13,9 @@ export default function CustomLink(props : any){
           {props.children}
         </Link>
       );
+    }
+    if(isAnchorLink){
+      return <a {...props} />
     }
   
     return <a target="_blank" {...props} />;
